310dda54f648bccc9c707c44c699930bb0e83015
windalfin <windalfinc@gmail.com>
2017-02-04 00:21:04 +0700

    move index and style to public folder, upload app.js that sendFile('index.html') on port 3000

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path =require("path");
+var app = express();
+
+app.get('/message', function(req, res) {
+    res.send('Hello World!')
+    });
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+    });
+
+app.listen(3000, function() {
+    console.log('helloworld app listening on port 3000!');
+    });
