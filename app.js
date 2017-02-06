2d58a9dfdcdc142c93fea615740d0933cdf5675e
windalfin <windalfinc@gmail.com>
2017-02-07 00:29:57 +0700

    changed app.js to use express.static

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path =require("path");
 var app = express();
 
+app.use(express.static(__dirname+ '/public'));
+
 app.get('/message', function(req, res) {
     res.send('Hello World!')
     });
@@ -9,6 +11,7 @@ app.get('/message', function(req, res) {
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
     });
+
 
 app.listen(3000, function() {
     console.log('helloworld app listening on port 3000!');
